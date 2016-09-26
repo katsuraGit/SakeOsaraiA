1f842e92c3a67dd5bc06c1f828d9e27929f49e3e
katsuraGit <kaito@microsoft.com>
2016-09-26 10:02:18 +0900

    ListViewに追加する

--- SakeOsaraiA/MainPage.xaml.cs
+++ SakeOsaraiA/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using System.Runtime.Serialization;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +29,20 @@ namespace SakeOsaraiA
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.textBox.Text == "お酒の名前を入力してください")
+            {
+                await new MessageDialog("ちゃんと入力してね！").ShowAsync();
+            }
+            else
+            {
+                this.listView.Items.Add(this.textBox.Text);
+             }
+
+            //            this.listView.Items.Add(this.textBox.Text);
         }
     }
 }
